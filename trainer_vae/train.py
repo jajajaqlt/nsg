bb4df1af2ce017fab09b5acb5279cd307bcde69b
jajajaqlt <724272859@qq.com>
2022-10-24 22:49:06 -0700

    extracts vectors of all evidences from saved checkpoints

--- trainer_vae/train.py
+++ trainer_vae/train.py
@@ -33,6 +33,18 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_" \
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
+# temp_labels, temp_nums = get_igmm_ins(apicalls[j], target_api[j], igmm_api_dict, 'api')
+def add_igmm_ins(ev_labels, ev_nums, igmm_dict, prefix, ins_labels, ins_nums):
+    # labels [10], nums [[10*256]]
+    temp_labels = []
+    temp_nums = []
+    nonzero_count = np.count_nonzero(ev_labels)
+    temp_labels = [prefix + igmm_dict[ev_labels[i]] for i in range(nonzero_count)]
+    temp_nums = [ev_nums[i] for i in range(nonzero_count)]
+    ins_labels.extend(temp_labels)
+    ins_nums.extend(temp_nums)
+
+
 def train(clargs):
     config_file = os.path.join(clargs.continue_from, 'config.json') \
         if clargs.continue_from is not None \
@@ -42,6 +54,14 @@ def train(clargs):
 
     loader = Loader(clargs.data, config)
     model = Model(config)
+
+    # adds api, kw and type dicts with indices as keys for igmm help function
+    try:
+        igmm_api_dict = {y: x for x, y in config.vocab.api_dict.items()}
+        igmm_type_dict = {y: x for x, y in config.vocab.type_dict.items()}
+        igmm_kw_dict = {y: x for x, y in config.vocab.kw_dict.items()}
+    except:
+        import pdb; pdb.set_trace()
 
     logger = create_logger(os.path.join(clargs.save, 'loss_values.log'))
     logger.info('Process id is {}'.format(os.getpid()))
@@ -107,49 +127,129 @@ def train(clargs):
                     model.encoder.ev_miss_rate: config.ev_miss_rate,
                     model.decoder.program_decoder.ast_tree.drop_prob: config.decoder_drop_rate
                 })
+                
+                if i == 0:
+                    print('writing vectors')
+                    # adds code to store all vectors
+                    # also writes apicalls, types, keywords
+                    # import pdb; pdb.set_trace()
+                    # everything is 128*10*256, except the ret 128*256, datatypes all float
+                    target_api, target_types, target_kws, target_fp, \
+                    target_field, target_ret, target_method, target_class, \
+                    target_jd, target_surr = sess.run(
+                        [model.encoder.program_encoder.ast_mean_api.reshaper,
+                         model.encoder.program_encoder.ast_mean_types.reshaper,
+                         model.encoder.program_encoder.ast_mean_kws.reshaper,
+                         model.encoder.program_encoder.fp_mean_enc.reshaper,
+                         model.encoder.program_encoder.field_enc.reshaper,
+                         model.encoder.program_encoder.ret_mean_enc.latent_encoding,
+                         model.encoder.program_encoder.method_enc.reshaper,
+                         model.encoder.program_encoder.class_enc.reshaper,
+                         model.encoder.program_encoder.jd_enc.reshaper,
+                         model.encoder.program_encoder.surr_enc.layer3],
+                        feed_dict=feed_dict)
 
-                # run the optimizer
-                loss, ast_loss, \
-                ast_gen_loss_concept, ast_gen_loss_api, \
-                ast_gen_loss_type, ast_gen_loss_clstype, ast_gen_loss_var, \
-                ast_gen_loss_op, ast_gen_loss_method, \
-                kl_loss, _, sigma = \
-                    sess.run([model.loss, model.ast_gen_loss,
-                              model.ast_gen_loss_concept, model.ast_gen_loss_api,
-                              model.ast_gen_loss_type, model.ast_gen_loss_clstype,
-                              model.ast_gen_loss_var, model.ast_gen_loss_op, model.ast_gen_loss_method,
-                              model.KL_loss, model.train_op, model.encoder.program_encoder.sigmas], feed_dict=feed_dict)
+                    # save non-zero output data into data_ls list
+                    # format [[[labels],[numbers]],...]
+                    data_ls = []
+                    data_ls_name = '102422ev_vec2/igmm_input_batch_' + str(i) + '_' + str(b)
+                    for j in range(config.batch_size):
+                        ins_labels = []
+                        ins_nums = []
+                        # 0. api
+                        add_igmm_ins(apicalls[j], target_api[j], igmm_api_dict, 'api_', ins_labels, ins_nums)
+                        # 1. types
+                        add_igmm_ins(types[j], target_types[j], igmm_type_dict, 'types_', ins_labels, ins_nums)
+                        # 2. kws
+                        add_igmm_ins(keywords[j], target_kws[j], igmm_kw_dict, 'kws_', ins_labels, ins_nums)
+                        # 3. fp
+                        add_igmm_ins(fp_in[j], target_fp[j], igmm_type_dict, 'fp_', ins_labels, ins_nums)
+                        # 4. field
+                        add_igmm_ins(fields[j], target_field[j], igmm_type_dict, 'field_', ins_labels, ins_nums)
+                        # 5. ret
+                        add_igmm_ins([ret_type[j]], [target_ret[j]], igmm_type_dict, 'ret_', ins_labels, ins_nums)
+                        # 6. method
+                        add_igmm_ins(method[j], target_method[j], igmm_kw_dict, 'method_', ins_labels, ins_nums)
+                        # 7. class
+                        add_igmm_ins(classname[j], target_class[j], igmm_kw_dict, 'class_', ins_labels, ins_nums)
+                        # 8. jd
+                        add_igmm_ins(javadoc_kws[j], target_jd[j], igmm_kw_dict, 'jd_', ins_labels, ins_nums)
+                        # 9. surr
+                        # surr_method change from 128 10 3 int to 128 10 1 int
+                        surr_method_first = [m[0] for m in surr_method[j]]
+                        add_igmm_ins(surr_method_first, target_surr[j], igmm_kw_dict, 'surr_', ins_labels, ins_nums)
+                        data_ls.append([ins_labels, ins_nums])
+                    # dataypes all int
+                    # 0
+                    # model.apicalls: apicalls 128 10
+                    # 1
+                    # model.types: types 128 10
+                    # 2
+                    # model.keywords: keywords 128 10
+                    # 3
+                    # model.formal_param_inputs: fp_in 128 10
+                    # 4
+                    # model.field_inputs: fields 128 10
+                    # 5
+                    # model.return_type: ret_type 128
+                    # 6
+                    # model.method: method 128 3
+                    # 7
+                    # model.classname: classname 128 3
+                    # 8
+                    # model.javadoc_kws: javadoc_kws 128 10
+                    # 9
+                    # model.surr_method: surr_method 128 10 3
+                    # import pdb; pdb.set_trace()
+                    np.save(data_ls_name, data_ls, allow_pickle=True)
 
-                avg_loss += np.mean(loss)
-                avg_ast_loss += np.mean(ast_loss)
+                    # import pdb; pdb.set_trace()
 
-                avg_ast_gen_loss_concept += np.mean(ast_gen_loss_concept)
-                avg_ast_gen_loss_method += np.mean(ast_gen_loss_method)
-                avg_ast_gen_loss_api += np.mean(ast_gen_loss_api)
-                avg_gen_loss_type += np.mean(ast_gen_loss_type)
-                avg_gen_loss_clstype += np.mean(ast_gen_loss_clstype)
-                avg_ast_gen_loss_var += np.mean(ast_gen_loss_var)
-                avg_ast_gen_loss_op += np.mean(ast_gen_loss_op)
 
-                avg_kl_loss += np.mean(kl_loss)
+                # # run the optimizer
+                # loss, ast_loss, \
+                # ast_gen_loss_concept, ast_gen_loss_api, \
+                # ast_gen_loss_type, ast_gen_loss_clstype, ast_gen_loss_var, \
+                # ast_gen_loss_op, ast_gen_loss_method, \
+                # kl_loss, _, sigma = \
+                #     sess.run([model.loss, model.ast_gen_loss,
+                #               model.ast_gen_loss_concept, model.ast_gen_loss_api,
+                #               model.ast_gen_loss_type, model.ast_gen_loss_clstype,
+                #               model.ast_gen_loss_var, model.ast_gen_loss_op, model.ast_gen_loss_method,
+                #               model.KL_loss, model.train_op, model.encoder.program_encoder.sigmas], feed_dict=feed_dict)
 
-                step = i * config.num_batches + b
-                if step % config.print_step == 0:
-                    logger.info('{}/{} (epoch {}) '
-                                'loss: {:.3f}, gen loss: {:.3f}, '
-                                'gen loss concept: {:.3f}, gen loss api: {:.3f}, '
-                                'gen loss type: {:.3f}, gen loss clstype: {:.3f}, gen loss var: {:.3f}, '
-                                'gen loss op: {:.3f}, gen loss method: {:.3f}, '
-                                'KL loss: {:.3f}. '
-                                .format(step,
-                                        config.num_epochs * config.num_batches,
-                                        i + 1, avg_loss / (b + 1), avg_ast_loss / (b + 1),
-                                        avg_ast_gen_loss_concept / (b + 1), avg_ast_gen_loss_api / (b + 1),
-                                        avg_gen_loss_type / (b + 1), avg_gen_loss_clstype / (b + 1),
-                                        avg_ast_gen_loss_var / (b + 1),
-                                        avg_ast_gen_loss_op / (b + 1), avg_ast_gen_loss_method / (b + 1),
-                                        avg_kl_loss / (b + 1)))
-                    logger.info('{}'.format([truncate_two_decimals(s) for s in sigma]))
+                # avg_loss += np.mean(loss)
+                # avg_ast_loss += np.mean(ast_loss)
+
+                # avg_ast_gen_loss_concept += np.mean(ast_gen_loss_concept)
+                # avg_ast_gen_loss_method += np.mean(ast_gen_loss_method)
+                # avg_ast_gen_loss_api += np.mean(ast_gen_loss_api)
+                # avg_gen_loss_type += np.mean(ast_gen_loss_type)
+                # avg_gen_loss_clstype += np.mean(ast_gen_loss_clstype)
+                # avg_ast_gen_loss_var += np.mean(ast_gen_loss_var)
+                # avg_ast_gen_loss_op += np.mean(ast_gen_loss_op)
+
+                # avg_kl_loss += np.mean(kl_loss)
+
+                # step = i * config.num_batches + b
+                # if step % config.print_step == 0:
+                #     logger.info('{}/{} (epoch {}) '
+                #                 'loss: {:.3f}, gen loss: {:.3f}, '
+                #                 'gen loss concept: {:.3f}, gen loss api: {:.3f}, '
+                #                 'gen loss type: {:.3f}, gen loss clstype: {:.3f}, gen loss var: {:.3f}, '
+                #                 'gen loss op: {:.3f}, gen loss method: {:.3f}, '
+                #                 'KL loss: {:.3f}. '
+                #                 .format(step,
+                #                         config.num_epochs * config.num_batches,
+                #                         i + 1, avg_loss / (b + 1), avg_ast_loss / (b + 1),
+                #                         avg_ast_gen_loss_concept / (b + 1), avg_ast_gen_loss_api / (b + 1),
+                #                         avg_gen_loss_type / (b + 1), avg_gen_loss_clstype / (b + 1),
+                #                         avg_ast_gen_loss_var / (b + 1),
+                #                         avg_ast_gen_loss_op / (b + 1), avg_ast_gen_loss_method / (b + 1),
+                #                         avg_kl_loss / (b + 1)))
+                #     logger.info('{}'.format([truncate_two_decimals(s) for s in sigma]))
+
+            import sys; sys.exit()
 
             if (i + 1) % config.checkpoint_step == 0:
                 checkpoint_dir = os.path.join(clargs.save, 'model_decoder_recont{}.ckpt'.format(i + 1))
